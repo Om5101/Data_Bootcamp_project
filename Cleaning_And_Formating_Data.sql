aa5f010e0db8a0eaf326209a5b31b7dc5b835c40
OM DINESH SHINDE <98743273+Om5101@users.noreply.github.com>
2025-11-05 22:52:07 +0530

    Update Cleaning_And_Formating_Data.sql

--- Cleaning_And_Formating_Data.sql
+++ Cleaning_And_Formating_Data.sql
@@ -8,11 +8,6 @@ SELECT
 	CampaignID,  -- campaign id number
     ProductID,  -- product id number
     UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType,  -- fixing socialmedia to social media and making all capital letters
-    LEFT(ViewsClicksCombined, LOCATE('-', ViewsClicksCombined) - 1) AS Views,  -- getting views from combined column (before the dash)
-    RIGHT(ViewsClicksCombined, LENGTH(ViewsClicksCombined) - LOCATE('-', ViewsClicksCombined)) AS Clicks,  -- getting clicks from combined column (after the dash)
-    Likes,  -- number of likes
-    -- formatting date to dd.mm.yyyy format
-    DATE_FORMAT(EngagementDate, '%d.%m.%Y') AS EngagementDate  -- converting date to dd.mm.yyyy format
 FROM 
     engagement_data  -- from engagement data table
 WHERE 
